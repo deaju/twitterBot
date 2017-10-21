253ab319f00fd84a4e9ce5fd2371d172bd5a210c
unknown <ganbaru.wakenai@gmail.com>
2017-10-21 13:25:10 +0900

    [add] insert to postgres

--- calcProgress.py
+++ calcProgress.py
@@ -3,7 +3,10 @@
 import os
 import re
 
+from datetime import datetime
 import twitter
+import psycopg2
+
 
 oath_key_dict = {
     "consumer_key": "tvYsqXcTYbxFtt9RAE3hElL7H",
@@ -12,7 +15,38 @@ oath_key_dict = {
     "access_token_secret": "ztebRO91VhlYGNyihQpJBllf9sZhm4WYVgcEBsQ5stuMJ"
 }
 
+def connectPostgres():
+    parse.uses_netloc.append("postgres")
+    url = parse.urlparse(os.environ["DATABASE_URL"])
 
+    conn = psycopg2.connect(
+        database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
+    )
+    return conn
+
+def storeProgress(conn,progress):
+    date=datetime.now().strftime("%Y-%m-%d")
+    progress=selectProgress(conn,date)
+    if len(progress) == 0:
+        cur=conn.cursor()
+        cur.execute('INSERT INTO nagoyan_sakura VALUES (%s,%s)',[date,progress])
+        cur.commit()
+    else:
+        cur=conn.cursor()
+        cur.execute('UPDATE nagoyan_sakura SET progres=(%s) WHERE date=(%s)',[progress,date])
+        cur.commit()
+    return
+        
+    
+def selectProgress(conn,date):
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM nagoyan_sakura where date=(%s)',[date])
+    progress = cur.fetchall()
+    return progress
 
 def getAuth():
     auth = twitter.OAuth(consumer_key=oath_key_dict["consumer_key"],
@@ -27,6 +61,7 @@ def getUserProgress(twitter,screen_name):
     user = twitter.users.lookup(screen_name=screen_name)
     user_name = user[0]['name']
     return re.search('([0-9]+).*/[0-9]*',user_name).group(1)
+
 
 
 
