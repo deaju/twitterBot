d8f823e734951d1cbea75db09cfd53f21efa31c2
unknown <ganbaru.wakenai@gmail.com>
2017-10-21 17:20:30 +0900

    [add] post tweet

--- calcProgress.py
+++ calcProgress.py
@@ -3,7 +3,7 @@
 import os
 import re
 
-from datetime import datetime
+from datetime import datetime, timedelta
 from urllib import parse
 import twitter
 import psycopg2
@@ -52,6 +52,13 @@ def selectProgress(conn,date):
     cur.close()
     return progress
 
+def deltaProgress(conn):
+    today = datetime.now().strftime("%Y-%m-%d")
+    yesterday = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")
+    todayProgress = selectProgress(conn,today)[0][1]
+    yesterdayProgress = selectProgress(conn,yesterday)[0][1]
+    return todayProgress - yesterdayProgress
+
 def getAuth():
     auth = twitter.OAuth(consumer_key=oath_key_dict["consumer_key"],
                   consumer_secret=oath_key_dict["consumer_secret"],
@@ -67,5 +74,22 @@ def getUserProgress(twitter,screen_name):
     return re.search('([0-9]+).*/[0-9]*',user_name).group(1)
 
 
+def postTweet(twitter,delta):
+    if delta > 0:
+        message="今日の進捗は{}話です".format(delta)
+    else if delta <= 0:
+        message="進捗ありません"
+    twitter.statuses.update(status=message
+
+def main():
+    twitter = getAuth()
+    screen_name = 'nagoyan240'
+    progress = getUserProgress(twitter,screen_name)
+    conn = connectPostgres()
+    storeProgress(conn,progress)
+
+    delta = deltaProgress(conn)
+    postTweet(twitter,delta)
+    
 
 
