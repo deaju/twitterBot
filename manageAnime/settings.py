8d076c27d1e70c02b8ef99b0c27bbaab9c10a549
root <ganbaru.wakenai@gmail.com>
2017-11-02 22:43:41 +0900

    fix

--- manageAnime/settings.py
+++ manageAnime/settings.py
@@ -131,7 +131,7 @@ ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
 import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
