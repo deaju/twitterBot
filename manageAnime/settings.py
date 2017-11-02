f0fff12b587b1569d602f080c1a66e45bf51fcea
root <ganbaru.wakenai@gmail.com>
2017-11-02 23:57:30 +0900

    [fix] migration

--- manageAnime/settings.py
+++ manageAnime/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from urllib import parse
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -68,15 +69,19 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'manageAnime.wsgi.application'
-
+url = parse.urlparse(os.environ["DATABASE_URL"])
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': url.path[1:],
+        'USER': url.username,
+        'PASSWORD' : url.password,
+        'HOST' : url.hostname,
+        'PORT' : url.port,
     }
 }
 
