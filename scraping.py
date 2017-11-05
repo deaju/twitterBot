6ed33fd6cf39467e4cfe05ecafa3f1b326dea7d6
root <ganbaru.wakenai@gmail.com>
2017-11-05 11:48:10 +0900

    fix

--- scraping.py
+++ scraping.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+import os
 import requests
 import re
 
