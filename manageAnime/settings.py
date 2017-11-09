9c82c6500b880fb5f5b3580e54ec5f4cd895aab8
root <ganbaru.wakenai@gmail.com>
2017-11-09 21:32:47 +0900

    add setting

--- manageAnime/settings.py
+++ manageAnime/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'showprogress.apps.Pollsconfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
