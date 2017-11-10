8c79152dc0820095d38e64c65d84eac8d9cbe5fc
root <ganbaru.wakenai@gmail.com>
2017-11-10 22:16:12 +0900

    fix

--- showprogress/views.py
+++ showprogress/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core import serializers
 
-from .models import History
+from .models import History,NagoyanSakura
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
@@ -15,7 +15,7 @@ def detail(request, title):
     return render(request, 'graph/index20.html', context)
 
 def sakura(request):
-    latest_history_list = History.objects.filter(title=title).order_by('date')[:4]
+    latest_history_list = NagoyanSakura.objects.filter().order_by('date')[:4]
     json_serializer = serializers.get_serializer('json')()
     data = simplejson.dumps(latest_history_list, ensure_ascii=False);
     context = {'latest_history_list': data}
