82b7a574194a1a06ac76f61b84a2548f3ff6daf8
root <ganbaru.wakenai@gmail.com>
2017-11-10 22:17:45 +0900

    fix

--- showprogress/views.py
+++ showprogress/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core import serializers
+from django.utils import simplejson
 
 from .models import History,NagoyanSakura
 
