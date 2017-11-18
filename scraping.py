ce0d8576e97c8bd3ace1264ed1ab94a525f516ea
root <ganbaru.wakenai@gmail.com>
2017-11-18 18:32:19 +0900

    store postgres

--- scraping.py
+++ scraping.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from datetime import datetime
+from urllib import parse
 import psycopg2
 import os
 import requests
