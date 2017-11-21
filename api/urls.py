4be296d077e4cc739439ddd3385b2de74e5ac742
root <ganbaru.wakenai@gmail.com>
2017-11-21 21:41:36 +0900

    fix urls

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,12 @@
 from rest_framework import routers
 from .views import HistoryViewSet, NagoyanSakuraSet, HistoryFilterViewSet
 from django.conf.urls import url
+from . import views
 
 
 router = routers.DefaultRouter()
-
 router.register(r'nagoyan', NagoyanSakuraSet)
 
-urlpatterns =[url(r'^progress/(?P<title>[\w]+)/$',HistoryFilterViewSet.as_view()),]
+urlpatterns =[
+    url(r'^v1/(?P<title>[\w]+)/$',HistoryFilterViewSet.as_view()),
+]
