d37da79485a2e26b485e2b5b4754947e6ff30870
root <ganbaru.wakenai@gmail.com>
2017-11-23 15:28:28 +0900

    add createHASh.py

---
+++ createHASH.py
@@ -0,0 +1,40 @@
+import hashlib
+
+def connectPostgres():
+    parse.uses_netloc.append("postgres")
+    url = parse.urlparse(os.environ["DATABASE_URL"])
+
+    conn = psycopg2.connect(
+        database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
+    )
+    return conn
+
+def selectProgress(conn):
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM nagoyan_sakura')
+    progress = cur.fetchall()
+    cur.close()
+    return progress
+
+def storeProgress(conn,url,title):
+    cur=conn.cursor()
+    cur.execute('INSERT INTO showprogress_history (url) VALUES (%s) where title = (%s)',[url,title])
+    conn.commit()
+    cur.close()
+    return
+
+def setURL(conn):
+    progress = selectProgress(conn)
+    for row in progress:
+        title = row['title']
+        url = hashlib.md5(title).hexdigest()
+        storeProgress(conn,url,title)
+
+def main():
+    conn = connectPostgres()
+    setURL(conn)
+
